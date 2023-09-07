9e1844190688044a95841c5bd062518121d523e0
jarcilla20 <124013303+jarcilla20@users.noreply.github.com>
2023-09-07 23:53:26 +0800

    Update search.py
    Implemented efficient user search based on multiple criteria, ensuring logical OR matching of any provided parameters.

--- phasebook/search.py
+++ phasebook/search.py
@@ -10,21 +10,57 @@ bp = Blueprint("search", __name__, url_prefix="/search")
 def search():
     return search_users(request.args.to_dict()), 200
 
-
 def search_users(args):
-    """Search users database
+    """Search users database 
 
     Parameters:
-        args: a dictionary containing the following search parameters:
-            id: string
-            name: string
-            age: string
-            occupation: string
+        args: A dictionary containing the following search parameters:
+            id: string 
+            name: string 
+            age: string 
+            occupation: string 
 
     Returns:
-        a list of users that match the search parameters
+        A list of users that match search parameters
     """
+    # Start with an empty list to store matching users
+    matching_users = []
 
-    # Implement search here!
+    # Extract search parameters from args dictionary
+    id = args.get('id')
+    name = args.get('name')
+    age = args.get('age')
+    occupation = args.get('occupation')
 
-    return USERS
+    def is_age_match(user_age, search_age):
+        return abs(user_age - search_age) <= 1
+    # Helper function for sorting based on matching priority
+    def matching_priority(user):
+        if user['id'] == id:
+            return 0
+        elif name and name.lower() in user['name'].lower():
+            return 1
+        elif age and is_age_match(int(user['age']), int(age)):
+            return 2
+        elif occupation and occupation.lower() in user['occupation'].lower():
+            return 3
+        else:
+            return 4
+
+    # Loop through the USERS data and filter based on search parameters
+    for user in USERS:
+        if id and str(user['id']) == id:
+            matching_users.append(user)
+        elif name and name.lower() in user['name'].lower():
+            matching_users.append(user)
+        elif age and is_age_match(int(user['age']), int(age)):
+            matching_users.append(user)
+        elif occupation and occupation.lower() in user['occupation'].lower():
+            matching_users.append(user)
+
+    # Sort matching_users based on matching priority
+    matching_users.sort(key=matching_priority)
+
+    return matching_users
+
+    #return USERS
